03c8d7715681c69c7b9bc2dac34989d2519da5aa
Stacy Zhao <stacyzhao@hotmail.com>
2016-08-18 15:37:42 -0400

    added task service

--- app.js
+++ app.js
@@ -1,41 +1,66 @@
 var app = angular.module('taskApp', []);
 
-app.controller('taskListController', function(){
-  this.tasks = tasks;
+app.service('TaskService', function(){
+  var id = 4;
+
+  var tasks = [
+    {
+      id: 0,
+      title: 'Dinner with jackie and brian',
+      description: '',
+      createdOn: 1397490980837,
+      complete: false
+    }, {
+      id: 1,
+      title: 'Lunch with mom',
+      description: '',
+      createdOn: 1397490980837,
+      complete: false
+    }, {
+      id: 2,
+      title: 'Clean and clear closet',
+      description: '',
+      createdOn: 1397490980837,
+      complete: false
+    }, {
+      id: 3,
+      title: 'hand in papers',
+      description: '',
+      createdOn: 1397490980837,
+      complete: false
+    }
+  ];
+
+  this.addTask = function(task){
+    task.id = id++;
+    task.createdOn = Date.now();
+    task.complete = false;
+    tasks.push(task);
+  };
+
+  this.getTask = function() {
+    return tasks;
+  }
+
 });
 
-app.controller('taskController', function (){
-  this.task = {};
-  this.addTask = function(title){
-    this.task.createdOn = Date.now();
-    this.task.complete = false;
-    tasks.push(this.task);
-    this.task = {};
+app.controller('TaskController', function($scope, TaskService){
+  $scope.tasks = TaskService.getTask();
+
+  $scope.addTask = function(){
+    TaskService.addTask($scope.newTask);
+    $scope.newTask = {};
   };
 });
 
-
-app.controller('deleteController', function(){
-  var filteredTask = _.filter(tasks, function(task){
-    return task.complete == true;
-  });
-});
-
-var tasks = [
-  {
-    title: 'Dinner with jackie and brian',
-    description: '',
-    createdOn: 1397490980837,
-    complete: false
-  }, {
-    title: 'Lunch with mom',
-    description: '',
-    createdOn: 1397490980837,
-    complete: false
-  }, {
-    title: 'Clean and clear closet',
-    description: '',
-    createdOn: 1397490980837,
-    complete: false
-  }
-];
+// app.controller('deleteController', function(){
+//   this.filterTask = function(){
+//
+//
+//     // this.evens = _.filter([1, 2, 3, 4, 5, 6], function(num){ return num % 2 == 0; });
+//     this.tasks = _.filter(tasks, function(task){
+//       return task.complete == false;
+//     });
+//     console.log(this.tasks)
+//   }
+// });
