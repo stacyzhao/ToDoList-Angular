33141816ef0233335c2c2af47f2d6049cd67dd76
Stacy Zhao <stacyzhao@hotmail.com>
2016-08-23 15:38:11 -0400

    added edit function

--- app.js
+++ app.js
@@ -8,40 +8,48 @@ app.service('TaskService', function(){
       id: 0,
       title: 'Dinner with jackie and brian',
       description: '',
-      createdOn: 1397490980837,
+      createdOn: 1394490980834,
       complete: false
     }, {
       id: 1,
       title: 'Lunch with mom',
       description: '',
-      createdOn: 1397490980837,
+      createdOn: 1397490980834,
       complete: false
     }, {
       id: 2,
       title: 'Clean and clear closet',
       description: '',
-      createdOn: 1397490980837,
+      createdOn: 1394490980834,
       complete: false
     }, {
       id: 3,
       title: 'hand in papers',
       description: '',
-      createdOn: 1397490980837,
+      createdOn: 1394490980834,
       complete: false
     }
   ];
 
   this.addTask = function(task){
-    task.id = id++;
-    task.createdOn = Date.now();
-    task.complete = false;
-    tasks.push(task);
+    if (task.id == null){
+      task.id = id++;
+      task.createdOn = Date.now();
+      task.complete = false;
+      tasks.push(task);
+    } else {
+      for (i in tasks) {
+        if (tasks[i].id == task.id){
+          tasks[i] = task
+        }
+      }
+    }
   };
 
-  this.editTask =function(id) {
-    for (task in tasks) {
-      if (tasks[task].id == id) {
-        tasks.push(task)
+  this.editTask = function(id){
+    for (i in tasks) {
+      if (tasks[i].id == id) {
+        return tasks[i]
       }
     }
   };
@@ -69,8 +77,12 @@ app.controller('TaskController', function($scope, TaskService){
     }
   };
 
+  $scope.editTask = function(id){
+    $scope.newTask = angular.copy(TaskService.editTask(id));
+  };
+
   $scope.deleteTask = function(id){
     TaskService.deleteTask(id);
     $scope.newTask = {};
-  }
+  };
 });
